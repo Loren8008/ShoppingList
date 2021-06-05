41bb62ba8d2f92e0c9681028591713dc0af8c6a3
gbochenska <81303640+gbochenska@users.noreply.github.com>
2021-06-05 13:32:47 +0200

    Update main.py
    added sort_list_by_category

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ import os
 import pickle
 
 
-     
-    
 def main():
     loaded_list_name = check_latest_name()
     loaded_list = load_shopping_list(loaded_list_name)
-   
+
     types_list = {
         'Bread': "Pastries",
         'White Cheese': "Dairy",
@@ -31,31 +29,33 @@ def main():
         'IceCream': 3.2
     }
     while 1:
-        loaded_list, product_list, loaded_list_name = selection_shopping_list(loaded_list, product_list, loaded_list_name)
+        loaded_list, product_list, loaded_list_name, types_list = selection_shopping_list(loaded_list, product_list,
+                                                                              loaded_list_name, types_list)
+
 
 def save_lst_name(loaded_list_name):
     file = open("last_lst.txt", "w")
     file.write("{} \n".format(loaded_list_name))
     file.close()
-    
+
+
 def save_shopping_list(loaded_list, loaded_list_name):
     file = open(loaded_list_name, "w")
     for x in loaded_list:
         file.write("{} {}\n".format(x, loaded_list[x]))
     file.close()
-      
+
 
 def check_latest_name():
     file = open("last_lst.txt", "r")
     lastest_name = file.read()
-    return lastest_name
     file.close()
-    
-    
- 
+    return lastest_name
+
+
 def load_shopping_list(list_to_load):
     loaded_list = {}
-    if (os.stat(list_to_load).st_size == 0):
+    if os.stat(list_to_load).st_size == 0:
         print("empty")
         list_to_load.close()
         return loaded_list
@@ -63,9 +63,19 @@ def load_shopping_list(list_to_load):
     with open(list_to_load, "r") as file:
         for line in file:
             key, value = line.split()
-            loaded_list[key]=int(value)
+            loaded_list[key] = int(value)
     file.close()
     return loaded_list
+
+def sort_list_by_category(loaded_list, types_list):
+    sorted_list = {}
+    category = input("Input category you want to sort by: ")
+    for key in loaded_list.keys():
+        if key in types_list.keys():
+            if types_list[key] == category:
+                sorted_list[key] = loaded_list[key]
+    print(sorted_list)
+    return sorted_list
 
 def modify_selected(loaded_list, selected_product):
     quantity = (int(input("insert product quantity\n")))
@@ -81,12 +91,14 @@ def rem_selected(loaded_list, selected_product):
     return loaded_list
 
 
-def add_new(loaded_list, product_list, new_product):
-    price = (int(input("Enter new product price\n")))
+def add_new(loaded_list, product_list, new_product, types_list):
+    price = (float(input("Enter new product price\n")))
     product_list[new_product] = price
     quantity = (int(input("insert product quantity\n")))
     loaded_list[new_product] = quantity
-    return loaded_list, product_list
+    type = input("insert product type\n")
+    types_list[new_product] = type
+    return loaded_list, product_list, types_list
 
 
 def add_existing(loaded_list, selected_product):
@@ -138,7 +150,9 @@ def add_list(file_name):
         os.chdir("..")
 
 
-def selection_add_or_remove(loaded_list, product_list):
+
+
+def selection_add_or_remove(loaded_list, product_list, types_list):
     add_or_remove_menu = ["1. Add", "2. Remove", "3. Go back\n"]
 
     for elem in add_or_remove_menu:
@@ -158,10 +172,10 @@ def selection_add_or_remove(loaded_list, product_list):
 
     elif user_choice == "3":
         clear()
-        selection_shopping_list(loaded_list, product_list)
+        selection_shopping_list(loaded_list, product_list, loaded_list, types_list)
 
 
-def selection_shopping_list(loaded_list, product_list, loaded_list_name):
+def selection_shopping_list(loaded_list, product_list, loaded_list_name, types_list):
     shopping_list_menu = ["1: Change current list", "2: Add/remove list", "3: Edit list", "4: Print list",
                           "5: Go back\n"]
     for item in shopping_list_menu:
@@ -172,10 +186,10 @@ def selection_shopping_list(loaded_list, product_list, loaded_list_name):
         print("bruh\n")
     elif "2" == user_choice:
         clear()
-        selection_add_or_remove(loaded_list, product_list)
+        selection_add_or_remove(loaded_list, product_list, types_list)
     elif "3" == user_choice:
         clear()
-        selection_shopping_list_edit(loaded_list, product_list)
+        selection_shopping_list_edit(loaded_list, product_list, types_list)
     elif "4" == user_choice:
         clear()
         prt_pos(loaded_list)
@@ -189,10 +203,10 @@ def selection_shopping_list(loaded_list, product_list, loaded_list_name):
     else:
         clear()
         print("bruhx2\n")
-    return loaded_list, product_list, loaded_list_name
+    return loaded_list, product_list, loaded_list_name, types_list
 
 
-def selection_shopping_list_edit(loaded_list, product_list):
+def selection_shopping_list_edit(loaded_list, product_list, types_list):
     shopping_list_edit_menu = ["enter position: Modify selected", "R: Remove", "B: Go back", "S: Sum up product prices",
                                "SB: Sum up product prices by category\n"]
     while 1:
@@ -215,16 +229,15 @@ def selection_shopping_list_edit(loaded_list, product_list):
             elif user_choice == "S":
                 print(sum_price_list(loaded_list, product_list))
             elif user_choice == "SB":
-                print(sum_price_list(loaded_list, product_list))
+                sorted_loaded_list = sort_list_by_category(loaded_list, types_list)
+                print(sum_price_list(sorted_loaded_list, product_list))
             elif user_choice == "B":
                 return loaded_list
             else:
-                loaded_list, product_list = add_new(loaded_list, product_list, user_choice)
+                loaded_list, product_list, tyepes_list= add_new(loaded_list, product_list, user_choice, types_list)
         else:
             print("Invalid input\n")
-        return loaded_list, product_list
-
-
+        return loaded_list, product_list, types_list
 
 
 clear = lambda: os.system('cls')  # on Windows System
