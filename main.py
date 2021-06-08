176c3dbba8dd89a9a123deba86163a1e288e0416
Michal Nedza <michalnedza@student.agh.edu.pl>
2021-06-09 00:46:19 +0200

    add new functionalities

--- main.py
+++ main.py
@@ -30,7 +30,58 @@ def main():
     }
     while 1:
         loaded_list, product_list, loaded_list_name, types_list = selection_shopping_list(loaded_list, product_list,
-                                                                              loaded_list_name, types_list)
+                                                                                          loaded_list_name, types_list)
+
+
+def add_product_to_product_list(product_list, product, value):
+    return edit_product_list(product_list, product, value)
+
+
+def remove_product_from_product_list(product_list, product):
+    del product_list[product]
+    return product_list
+
+
+def edit_product_list(product_list, product, value):
+    product_list[product] = value
+    return product_list
+
+
+def remove_product_from_types_list(types_list, product_remove):
+    del types_list[product_remove]
+    return types_list
+
+
+def add_new_type(type, types_list, new_product):
+    types_list[new_product] = type
+    return types_list
+
+
+def edit_types_list(types_list, product):
+    types_list[product] = input("Enter type of the product: ")
+    return types_list
+
+
+def change_list(loaded_list_name):
+    print("Change list from: ", loaded_list_name)
+    print("Existing files: ")
+    lists = print_files("Lists")
+    lst = input()
+    if (not lst or lst == "\n") or ((lst + ".txt") not in os.listdir("Lists")):
+        print("Incorrect list name")
+        print(lst)
+        return loaded_list_name, load_shopping_list(loaded_list_name)
+    file = open("last_lst.txt", "w")
+    file.writelines(lst + ".txt")
+    file.close()
+    return "{}.txt".format(lst), load_shopping_list("{}.txt".format(lst))
+
+
+def print_files(path):
+    files = os.listdir(path)
+    for i in files:
+        print(" -", (i.replace(".txt", "")))
+    return files
 
 
 def save_lst_name(loaded_list_name):
@@ -40,32 +91,50 @@ def save_lst_name(loaded_list_name):
 
 
 def save_shopping_list(loaded_list, loaded_list_name):
+    os.chdir("./Lists")
     file = open(loaded_list_name, "w")
     for x in loaded_list:
         file.write("{} {}\n".format(x, loaded_list[x]))
     file.close()
+    os.chdir("..")
 
 
 def check_latest_name():
+    latest_name = ""
+    if "last_lst.txt" not in os.listdir():
+        print("ERROR: last_lst.txt does not exist")
+        return ""
+
     file = open("last_lst.txt", "r")
-    lastest_name = file.read()
-    file.close()
+    if file.read() == ".txt" or "":
+        print("ERROR: file incorrect")
+        return ""
+    else:
+        lastest_name = file.read()
+        file.close()
+        print("Loaded list is: ", lastest_name)
     return lastest_name
 
 
 def load_shopping_list(list_to_load):
     loaded_list = {}
-    if os.stat(list_to_load).st_size == 0:
-        print("empty")
-        list_to_load.close()
+    os.chdir("./Lists")
+    if list_to_load == "":
+        print("no such list, please select other one")
+        os.chdir("..")
         return loaded_list
-    print(list_to_load)
+    if os.stat(list_to_load).st_size == 0:
+        print("no products in list")
+        os.chdir("..")
+        return loaded_list
     with open(list_to_load, "r") as file:
         for line in file:
             key, value = line.split()
             loaded_list[key] = int(value)
+    os.chdir("..")
     file.close()
     return loaded_list
+
 
 def sort_list_by_category(loaded_list, types_list):
     sorted_list = {}
@@ -76,6 +145,7 @@ def sort_list_by_category(loaded_list, types_list):
                 sorted_list[key] = loaded_list[key]
     print(sorted_list)
     return sorted_list
+
 
 def modify_selected(loaded_list, selected_product):
     quantity = (int(input("insert product quantity\n")))
@@ -93,11 +163,11 @@ def rem_selected(loaded_list, selected_product):
 
 def add_new(loaded_list, product_list, new_product, types_list):
     price = (float(input("Enter new product price\n")))
-    product_list[new_product] = price
+    product_list = add_product_to_product_list(product_list, new_product, price)
     quantity = (int(input("insert product quantity\n")))
     loaded_list[new_product] = quantity
     type = input("insert product type\n")
-    types_list[new_product] = type
+    types_list = add_new_type(type, types_list, new_product)
     return loaded_list, product_list, types_list
 
 
@@ -121,9 +191,9 @@ def prt_pos(loaded_list):
 
 
 def sum_price_list(list_to_sum, product_list):
-    sum_price = 0
+    sum_price = 0.0
     for product in list_to_sum:
-        sum_price += (list_to_sum[product] * product_list[product])
+        sum_price += (float(list_to_sum[product]) * product_list[product])
     return sum_price
 
 
@@ -135,7 +205,7 @@ def remove_list(file_name):
         loaded_list = {}
         return loaded_list
     except IOError:
-        print("File doesn't exist\n")
+        print("List doesn't exist\n")
         os.chdir("..")
 
 
@@ -146,50 +216,51 @@ def add_list(file_name):
         loaded_list = {}
         os.chdir("..")
     except IOError:
-        print("A file with that name already exist")
+        print("Such a list already exists")
         os.chdir("..")
-
-
 
 
 def selection_add_or_remove(loaded_list, product_list, types_list):
     add_or_remove_menu = ["1. Add", "2. Remove", "3. Go back\n"]
 
-    for elem in add_or_remove_menu:
-        print(elem)
-    user_choice = input("select option ")
-    if user_choice == "1":
-        clear()
-        print("Existing files: ", os.listdir("Lists"))
-        list_to_add = input("Type name of list to add: \n")
-        add_list(list_to_add)
+    while 1:
+        for elem in add_or_remove_menu:
+            print(elem)
+        user_choice = input("select option ")
+        if user_choice == "1":
+            clear()
+            print("Existing lists: ")
+            print_files("Lists")
+            list_to_add = input("Type name of list to add: \n")
+            add_list(list_to_add)
 
-    elif user_choice == "2":
-        clear()
-        print("Existing files: ", os.listdir("Lists"))
-        list_to_remove = input("Type name of list to remove: \n")
-        remove_list(list_to_remove)
+        elif user_choice == "2":
+            clear()
+            print("Existing lists: ")
+            print_files("Lists")
+            list_to_remove = input("Type name of list to remove: \n")
+            remove_list(list_to_remove)
 
-    elif user_choice == "3":
-        clear()
-        selection_shopping_list(loaded_list, product_list, loaded_list, types_list)
+        elif user_choice == "3":
+            clear()
+            return loaded_list, product_list, types_list
 
 
 def selection_shopping_list(loaded_list, product_list, loaded_list_name, types_list):
-    shopping_list_menu = ["1: Change current list", "2: Add/remove list", "3: Edit list", "4: Print list",
-                          "5: Go back\n"]
+    shopping_list_menu = ["1: Change current list (" + loaded_list_name.replace(".txt", "") + ")", "2: Add/remove list", "3: Edit list", "4: Print list",
+                          "5: Exit\n"]
     for item in shopping_list_menu:
         print(item)
     user_choice = (input("select option "))
     if "1" == user_choice:
         clear()
-        print("bruh\n")
+        loaded_list_name, loaded_list = change_list(loaded_list_name)
     elif "2" == user_choice:
         clear()
-        selection_add_or_remove(loaded_list, product_list, types_list)
+        loaded_list, product_list, types_list = selection_add_or_remove(loaded_list, product_list, types_list)
     elif "3" == user_choice:
         clear()
-        selection_shopping_list_edit(loaded_list, product_list, types_list)
+        loaded_list, product_list, types_list, loaded_list_name = selection_shopping_list_edit(loaded_list, product_list, types_list, loaded_list_name)
     elif "4" == user_choice:
         clear()
         prt_pos(loaded_list)
@@ -202,19 +273,23 @@ def selection_shopping_list(loaded_list, product_list, loaded_list_name, types_l
         exit()
     else:
         clear()
-        print("bruhx2\n")
     return loaded_list, product_list, loaded_list_name, types_list
 
 
-def selection_shopping_list_edit(loaded_list, product_list, types_list):
+def selection_shopping_list_edit(loaded_list, product_list, types_list, loaded_list_name):
     shopping_list_edit_menu = ["enter position: Modify selected", "R: Remove", "B: Go back", "S: Sum up product prices",
                                "SB: Sum up product prices by category\n"]
+    if not loaded_list_name:
+        temp = input("Enter list name: \n")
+        while not temp:
+            temp = input("Incorrect list name, try again: \n")
+        loaded_list_name = temp + ".txt"
     while 1:
         clear()
         prt_pos(loaded_list)
         for item in shopping_list_edit_menu:
             print(item)
-        user_choice = (input("select option "))
+        user_choice = (input("Select option or type the name of the product: "))
         if type(user_choice) == str:
             if user_choice in loaded_list:
                 loaded_list = modify_selected(loaded_list, user_choice)
@@ -235,9 +310,12 @@ def selection_shopping_list_edit(loaded_list, product_list, types_list):
                 return loaded_list
             else:
                 loaded_list, product_list, tyepes_list= add_new(loaded_list, product_list, user_choice, types_list)
+
+            save_shopping_list(loaded_list, loaded_list_name)
+
         else:
             print("Invalid input\n")
-        return loaded_list, product_list, types_list
+        return loaded_list, product_list, types_list, loaded_list_name
 
 
 clear = lambda: os.system('cls')  # on Windows System
